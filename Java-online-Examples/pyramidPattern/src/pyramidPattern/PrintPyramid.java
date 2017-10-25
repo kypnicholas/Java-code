0b491f452c1a06f93ab0400ca08fd049019a1d94
Nicolaos Kyprianou <nkyprianou11@hotmail.com>
2017-10-25 11:07:12 +0100

    added new exercises completed

--- Java-online-Examples/pyramidPattern/src/pyramidPattern/PrintPyramid.java
+++ Java-online-Examples/pyramidPattern/src/pyramidPattern/PrintPyramid.java
@@ -5,16 +5,35 @@ public class PrintPyramid {
 	public static void main(String args[]) { 
 		System.out.println("Pyramid of stars: "); 
 		drawPyramidofStars(); 
+		System.out.println("Inverted Pyramid of stars: "); 
+		drawInvertedPyramidofStars(); 
 		System.out.println("Pyramid of numbers: "); 
 		drawPyramidOfNumbers(); } 
 	
-	//This method draws a pyramid pattern using asterisk character
+	//This method draws a pyramid pattern using asterisks
 	public static void drawPyramidofStars() { 
-		for (int i = 0; i < 5; i++) { 
-			for (int j = 0; j < 5 - i; j++) { 
+		for (int rows = 0; rows < 5; rows++) { 
+			for (int spaces = 0; spaces < 5 - rows; spaces++) { 
 				System.out.print(" "); 
 			} 
-			for (int k = 0; k <= i; k++) { 
+			for (int stars = 0; stars <= rows; stars++) { 
+				System.out.print("* "); 
+			} 
+			System.out.println(); 
+		} 
+	} 
+	
+	//This method draws an inverted pyramid pattern using asterisks
+	public static void drawInvertedPyramidofStars() { 
+		
+		int i, spaces, stars =0;
+		int rows =5;
+		for (i = rows; i >= 1; i--) 
+		{ 
+			for (spaces = 0; spaces <= rows-i; spaces++) { 			
+				System.out.print(" "); 
+			} 
+			for (stars = 0; stars <= rows-spaces; stars++) { 
 				System.out.print("* "); 
 			} 
 			System.out.println(); 
@@ -23,12 +42,12 @@ public class PrintPyramid {
 	
 	//This method draws a pyramid of numbers
 	public static void drawPyramidOfNumbers() { 
-		for (int i = 0; i < 5; i++) { 
-			for (int j = 0; j < 5 - i; j++) { 
+		for (int rows = 0; rows < 5; rows++) { 
+			for (int spaces = 0; spaces < 5 - rows; spaces++) { 
 				System.out.print(" "); 
 			} 
-			for (int k = 0; k <= i; k++) { 
-				System.out.print(k + " "); 
+			for (int numbers = 0; numbers <= rows; numbers++) { 
+				System.out.print(numbers + " "); 
 			} 
 			System.out.println(); 
 		} 
